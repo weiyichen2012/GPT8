d9381f7db2320e61aec00fb15a3349a4b6c41aba
ethepherein <ethepherein@sjtu.edu.cn>
2024-03-27 14:37:12 +0800

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import prediction_train.train as prediction_train
+
+if __name__ == '__main__':
+  baseDir = os.path.dirname(os.path.abspath(__file__))
+  emotionList = ['happy', 'sad', 'neutral']
+  sceneList = ['study', 'gaming', 'idle']
+
+  prediction_runner = prediction_train.PredictionRunner(baseDir, emotionList, sceneList)
+  prediction_runner.train()
+  # prediction_runner.eval_network()
+
+# print(baseDir)
--- prediction_train/train.py
+++ prediction_train/train.py
@@ -1,24 +1,59 @@
 import numpy as np
 import warnings
-from model import *
 import pandas as pd
 import torch
 import torch.optim as optim
 import os
-# from tqdm import tqdm
 import numpy.random as random
 import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+img2me = lambda x, y : torch.mean((x - y) ** 2)
+
+class NeuralNetwork(nn.Module):
+
+    def __init__(self, input_dim, output_dim, D=5, W=200, skips=[2]):
+        super().__init__()
+
+        self.skips = skips
+        self.network_linears = nn.ModuleList(
+            [nn.Linear(input_dim, W)] +
+            [nn.Linear(W, W) if i not in skips else nn.Linear(W + input_dim, W)
+             for i in range(D - 1)]
+        )
+
+        ## output head, 2 for amplitude and phase
+        self.network_output = nn.Linear(W, output_dim)
+
+    def forward(self, network_input):
+        
+        x = network_input
+
+        for i, layer in enumerate(self.network_linears):
+            x = F.relu(layer(x))
+            if i in self.skips:
+                x = torch.cat([network_input, x], -1)
+
+        outputs = self.network_output(x)    # (batch_size, 2)
+        return outputs
+
 
 emotionList = ['happy', 'sad', 'neutral']
 sceneList = ['study', 'gaming', 'idle']
 
 class PredictionNeuralRunner():
-  def __init__(self) -> None:
+  def __init__(self, baseDir, emotionList, sceneList) -> None:
+    self.baseDir = baseDir
+    self.emotionList = emotionList
+    self.sceneList = sceneList
+
     self.devices = torch.device('cpu')
     self.total_iterations = 1000
     self.PastListLen = 10
-    self.input_dim = (len(emotionList) + len(sceneList)) * 10
-    self.output_dim = len(emotionList)
+    self.input_dim = (len(self.emotionList) + len(self.sceneList)) * 10
+    self.output_dim = len(self.emotionList)
 
     self.network = NeuralNetwork(self.input_dim, self.output_dim)
     params = list(self.network.parameters())
@@ -32,11 +67,12 @@ class PredictionNeuralRunner():
     self.current_iteration = 1
     self.load_checkpoints()
     self.save_freq = 100
-    self.read_info()
+    self.read_data()
     return
   
-  def read_info(self):
-    df = pd.read_csv('input.csv')
+  def read_data(self):
+    fileName = os.path.join(self.baseDir, 'input.csv')
+    df = pd.read_csv(fileName)
     self.train_input = []
     self.train_label = []
 
@@ -57,30 +93,33 @@ class PredictionNeuralRunner():
       self.train_input.append([])
       for j in range(0, PastListLen):
         currI = i + j
-        for k in range(0, len(emotionList)):
-          if df['emotion'][currI] == emotionList[k]:
+        for k in range(0, len(self.emotionList)):
+          if df['emotion'][currI] == self.emotionList[k]:
             self.train_input[i].append(1)
           else:
             self.train_input[i].append(0)
 
       for j in range(0, PastListLen):
         currI = i + j
-        for k in range(0, len(sceneList)):
-          if df['scene'][currI] == sceneList[k]:
+        for k in range(0, len(self.sceneList)):
+          if df['scene'][currI] == self.sceneList[k]:
             self.train_input[i].append(1)
           else:
             self.train_input[i].append(0)
       
       currI = i + 1
       self.train_label.append([])
-      for k in range(0, len(emotionList)):
-        if df['emotion'][currI] == emotionList[k]:
+      for k in range(0, len(self.emotionList)):
+        if df['emotion'][currI] == self.emotionList[k]:
           self.train_label[i].append(1)
         else:
           self.train_label[i].append(0)
 
-    self.test_input = self.train_input[-testLen:]
-    self.test_label = self.train_label[-testLen:]
+    # self.test_input = self.train_input[-testLen:]
+    # self.test_label = self.train_label[-testLen:]
+    self.test_input = self.train_input
+    self.test_label = self.train_label      
+    
     self.train_input = self.train_input[:-testLen]
     self.train_label = self.train_label[:-testLen]
       
@@ -89,16 +128,20 @@ class PredictionNeuralRunner():
     self.test_input = torch.tensor(self.test_input, dtype=torch.float32)
     self.test_label = torch.tensor(self.test_label, dtype=torch.float32)
 
-  def load_checkpoints(self):
-    ckptsdir = os.path.join('ckpts')
+  def load_checkpoints(self, specCkptFile=None):
+    ckptsdir = os.path.join(self.baseDir, 'ckpts')
     if not os.path.exists(ckptsdir):
         os.makedirs(ckptsdir)
     ckpts = [os.path.join(ckptsdir, f) for f in sorted(os.listdir(ckptsdir)) if 'tar' in f]
     print("Found ckpts:", ckpts)
 
     if len(ckpts) > 0:
-      ckpt_path = ckpts[-1]
-      print('Loading ckpt %s', ckpt_path)
+      if specCkptFile is not None and specCkptFile in ckpts:
+        ckpt_path = os.path.join(ckptsdir, specCkptFile)
+        print('Loading specific ckpt %s', ckpt_path)
+      else:
+        ckpt_path = ckpts[-1]
+        print('Loading ckpt %s', ckpt_path)
       ckpt = torch.load(ckpt_path, map_location=self.devices)
 
       self.network.load_state_dict(ckpt['network_state_dict'])
@@ -110,7 +153,7 @@ class PredictionNeuralRunner():
 
 
   def save_checkpoint(self):
-    ckptsdir = os.path.join('ckpts')
+    ckptsdir = os.path.join(self.baseDir, 'ckpts')
     model_lst = [x for x in sorted(os.listdir(ckptsdir)) if x.endswith('.tar')]
     if len(model_lst) > 2:
       os.remove(ckptsdir + '/%s' % model_lst[0])
@@ -128,11 +171,8 @@ class PredictionNeuralRunner():
 
 
   def train(self):
-    # print("Start training. Current Iteration:%d", self.current_iteration)
     t1 = time.time()
     for i in range(0, self.total_iterations):
-        # with tqdm(total=self.total_iterations, desc=f"Iteration {self.current_iteration}/{self.total_iterations}") as pbar:
-            # for train_input, train_label in self.train_iter:
         if self.current_iteration > self.total_iterations:
           break
           
@@ -145,15 +185,10 @@ class PredictionNeuralRunner():
         self.cosine_scheduler.step()
         self.current_iteration += 1
 
-          # self.writer.add_scalar('Loss/loss', loss, self.current_iteration)
-          # pbar.update(1)
-          # pbar.set_description(f"Iteration {self.current_iteration}/{self.total_iterations}")
-          # pbar.set_postfix_str('loss = {:.6f}, lr = {:.6f}'.format(loss.item(), self.optimizer.param_groups[0]['lr']))
         print(f"Iteration {self.current_iteration}/{self.total_iterations} loss = {loss.item()}, lr = {self.optimizer.param_groups[0]['lr']}")
 
         if i % 100 == 0:
             ckptname = self.save_checkpoint()
-              # pbar.write('Saved checkpoints at {}'.format(ckptname))
     t2 = time.time()
     print("\nTraining take {:f} seconds. Current Iteration:{:d}, Current Loss: {:f}.\n".format(t2 - t1, self.current_iteration, loss.item()))
 
@@ -178,12 +213,64 @@ class PredictionNeuralRunner():
         mismatch_sum += 1
       else:
         match_sum += 1
-      print(emotionList[train_index], emotionList[actual_index], "Confidence ", result_values[train_index])
+      print(self.emotionList[train_index], self.emotionList[actual_index], "Confidence ", result_values[train_index])
     print("Match: ", match_sum, "Mismatch: ", mismatch_sum)
+  
+  def predict(self, inputList):
+    result = self.network.forward(inputList)
+    result = result.detach().numpy()
+    result_values = list(result[0])
+    train_index = max(range(len(result_values)), key=result_values.__getitem__)
+    print("Predicted: ", self.emotionList[train_index], "Confidence: ", result_values[train_index])
+    return True, self.emotionList[train_index]
+
+class PredictionRunner():
+  def __init__(self, baseDir, emotionList, sceneList) -> None:
+    self.baseDir = os.path.join(baseDir, 'prediction_train')
+    self.emotionList = emotionList
+    self.sceneList = sceneList
+
+    self.runner = PredictionNeuralRunner(self.baseDir, self.emotionList, self.sceneList)
+    return
+  
+  def train(self):
+    self.runner.train()
+    return
+  
+  def eval_network(self):
+    self.runner.eval_network()
+    return
+  
+  def predict(self, emotionList, sceneList):
+    if len(emotionList) < self.runner.PastListLen or len(sceneList) < self.runner.PastListLen:
+      return False, "Not enough data"
+    emotionList = emotionList[-self.runner.PastListLen:]
+    sceneList = sceneList[-self.runner.PastListLen:]
+    inputList = []
+    for i in range(0, len(emotionList)):
+      for j in range(0, len(self.emotionList)):
+        if emotionList[i] == self.emotionList[j]:
+          inputList.append(1)
+        else:
+          inputList.append(0)
+    
+    for i in range(0, len(sceneList)):
+      for j in range(0, len(self.sceneList)):
+        if sceneList[i] == self.sceneList[j]:
+          inputList.append(1)
+        else:
+          inputList.append(0)
+    
+    inputList = torch.tensor(inputList, dtype=torch.float32)
+    result, emotion = self.runner.predict(inputList)
+    return result, emotion
+
+
 
 if __name__ == '__main__':
   warnings.simplefilter(action='ignore', category=FutureWarning)
-  worker = PredictionNeuralRunner()
+  runner = PredictionRunner()
+  # worker = PredictionNeuralRunner()
   # np.random.seed()
   # worker.train()
-  worker.eval_network()
+  # worker.eval_network()
