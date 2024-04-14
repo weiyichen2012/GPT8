4b300e3f6f67a4e9d040dba3318ac460597bb71b
Aiden-Wei <1270161395@qq.com>
2024-04-14 16:52:51 +0800

    update

--- servo/servo.py
+++ servo/servo.py
@@ -1,11 +1,11 @@
-import rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BMC)
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(24, GPIO.OUT)
 
-pwm=GPIO.PWM(11, 50)
+pwm=GPIO.PWM(24, 50)
 
 pwm.start(0)
-
-pwm.changeDutyCycle(5)
+        
+pwm.ChangeDutyCycle(7.5)
