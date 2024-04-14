104bab0b603daf3fcedda4362319693b25e6feb8
ethepherein <ethepherein@sjtu.edu.cn>
2024-04-14 17:32:45 +0800

    update

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
   arm_control_runner = ArmControlRunner(baseDir, ifDebug=True)
   audio_detection_runner.start_regonize()
 
-  time.sleep(20)
+  time.sleep(10)
   emotion = getEmotion()
   # description  = emotion_detection_runner.get_emotion_description("picture.jpg")
   # print(description)
