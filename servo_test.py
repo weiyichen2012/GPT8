f0d0edb25a605d95c6319d08436f779dbf1b5696
不吃梨 <xuan@buchilidebijibendiannao.local>
2024-04-18 09:00:43 +0800

    update

---
+++ servo_test.py
@@ -0,0 +1,53 @@
+import os
+from audio_detection.main import AudioDetectionRunner
+from emotion_detection.gpt.emotion_detection import EmotionDetectionRunner
+from image_taker.image import ImageTakerRunner
+from armControl.armControl import ArmControlRunner
+from light.light import LightRunner
+from servo.servo import ServoRunner
+# from handRecognition.hand import HandRecognitionRunner
+import time
+from yeelight import *
+import random
+
+cheatEmotionDetection = True  # True 代表作弊，自动悲伤
+cheatTwoHands = False  # False 代表作弊，不识别
+
+emotionList = ['开心', '悲伤', '中立']
+ifTest = False
+
+
+def getEmotion(ifDebug=True):
+    sentence = "我很伤心"
+    possibility_text = emotion_detection_runner.get_emotion_text(sentence)
+    possibility_image = emotion_detection_runner.get_emotion_image("picture.jpg")
+
+    maxEmotion = emotionList[0]
+
+    maxPossibility = possibility_image[0] + possibility_text[0]
+
+    for i in range(1, len(emotionList)):
+        if possibility_image[i] + possibility_text[i] > maxPossibility:
+            maxEmotion = emotionList[i]
+            maxPossibility = possibility_image[i] + possibility_text[i]
+
+    if ifDebug:
+        print("Audio: ", sentence)
+        print("detection finished\n", possibility_text, possibility_image)
+        print("Emotion: ", maxEmotion, maxPossibility)
+
+    return maxEmotion
+
+
+if __name__ == '__main__':
+    baseDir = '/home/pi/GPT8/'
+    audio_detection_runner = AudioDetectionRunner(baseDir, ifDebug=False)
+    emotion_detection_runner = EmotionDetectionRunner(baseDir, ifDebug=False)
+    image_taker_runner = ImageTakerRunner(baseDir, ifDebug=True)  # 设成True打开拍照窗口
+    arm_control_runner = ArmControlRunner(baseDir, ifDebug=False)
+    servo_runner = ServoRunner(baseDir, ifDebug=False)
+    light_runner = LightRunner(baseDir, ifDebug=False)
+
+    arm_control_runner.moveArmFile('reset.d6a')
+    input()
+    servo_runner.moveByListAsync([180, 0, 180, 0, 180, 0, 180, 0, 180, 0], [2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
